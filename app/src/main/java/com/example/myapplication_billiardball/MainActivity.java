2c64a8ae1db1e67ecdccd390dfe662e420a5e83d
kim-hyunjoonge <yui3268@naver.com>
2022-04-16 14:52:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_billiardball/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication_billiardball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    BilliardBall ball;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        ball = new BilliardBall(this);
+        setContentView(ball);
+    }
+}
